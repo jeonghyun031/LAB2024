79ab5ad7ee9c94c7003e8062f0f6e281557402ee
Park-Jeong-Hyun <pjh1849@gmail.com>
2024-09-20 22:41:02 +0900

    modify code

--- python/test.py
+++ python/test.py
@@ -1,12 +1,1 @@
-def primeNumber(n):
-    prime = [i for i in range(0,n+1)]
-
-for i in prime:
-    for j in range(2,i):
-        if i % j == 0:
-            prime[i] = 0
-            break
-prime = [i for i in prime[3:] if i != 0]
-return prime
-
-primeNumber(50)
+print("Hello World")
