f3e6c749157f0be5b4c90afba056865510ab8941
Park-Jeong-Hyun <pjh1849@gmail.com>
2024-09-23 22:20:18 +0900

    Update push3

--- Arduino/push3.ino
+++ Arduino/push3.ino
@@ -2,13 +2,13 @@
 
 int LED = 13;                           //LED를 아두이노 13번 핀에 연결
 int SW =7;                              //푸시 버튼을 아두이노 7번 핀에 연결
-int num = 0;                                /* 푸시 버튼이 눌린 횟수를 저장한 int형
+int num = 0;                            /* 푸시 버튼이 눌린 횟수를 저장한 int형
                                             num을 선언*/
 boolean check = 0;                      //초기값 false로 설정
 
 void setup() {
     pinMode(LED, OUTPUT);               //13번핀(LED)을 출력으로 설정
-    pinMode(SW, INPUT);                  //7번 핀(SW)을 입력을 설정
+    pinMode(SW, INPUT);                  //7번 핀(SW)을 입력으로 설정
     Serial.begin(9600);                 // 시리얼 통신 초기화
 }   
 void loop() {
@@ -26,7 +26,7 @@ void loop() {
             num = num + 1;              //num에 저장된 값이 1만큼 증가
             check = true;               //변수 check에 저장된 값을 true로 변경
         }
-        num += 1;                       //푸시 버튼이 늘리면 num이 1씩 증가
+        num = num + 1;                       //푸시 버튼이 눌리면 num이 1씩 증가
         digitalWrite(LED, HIGH);        //13번 핀(LED)으로 HIGH(5V)를 출력
         Serial.println("SW : ON");      //시리얼 모니터에 "SW : ON" 출력 후 줄 바꿈
         Serial.print("num =");          //시리얼 모니터에 "num = " 문자열 출력
