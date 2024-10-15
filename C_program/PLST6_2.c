b6a9d0dbec033031181b497f294b50366e4f04d0
jeonghyun031 <pjh1849@gmail.com>
2024-10-15 20:53:43 +0900

    add C
    프로그래밍언어구조론 실습 코드 업로드

---
+++ C_program/PLST6_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x, *ptr = NULL;
+    x = 10;
+    ptr = (int *)malloc(sizeof(int));
+
+    if (ptr == NULL) {
+        printf("동적 메모리 할당 오류\n");
+        exit(1);
+    }
+    printf("정수 입력 >>");
+    scanf("%d", ptr);
+
+    printf("x = %d, *ptr = %d\n", x, *ptr);
+    free(ptr);
+}
