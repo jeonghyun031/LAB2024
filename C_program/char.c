3051e093f1c4c8e134d63085e595f88115c179b2
jeonghyun031 <pjh1849@gmail.com>
2024-10-29 22:45:23 +0900

    add C
    프로그래밍언어구조론 실습 코드.
    문자와 문자열 선언

---
+++ C_program/char.c
@@ -0,0 +1,11 @@
+//문자와 문자열 선언
+#include <stdio.h>
+int main() {
+    char a = 'A';
+    char b[] = "Programming Language";
+    char *c = "SON LEE KYEONG";
+
+    printf("%c\n", a);
+    printf("%s\n", b);
+    printf("%s\n", c);
+}
