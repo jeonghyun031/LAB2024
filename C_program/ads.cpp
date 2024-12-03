7ddb6918fc9d8fcdb2927101267c1d6d9851d259
jeonghyun031 <pjh1849@gmail.com>
2024-12-03 15:09:31 +0900

    update cpp

--- C_program/ads.cpp
+++ C_program/ads.cpp
@@ -1,16 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-template <class T>
-T big(T a, T b) {
+int big(int a, int b) {
     if ( a > b)
         return a;
     else
         return b;
 }
-int main() {
-    int a=20, b=50;
-    char c = 'a', d ='z';
+
+char big(char a, char b) {
+    if ( a > b)
+        return a;
+    else
+        return b;
+}
+int main()
+{
+    int a = 20, b=50;
+    char c = 'a' , d='z';
     cout << "big(20,50)의 결과는 " << big(a,b) << endl;
     cout << "big('a','z')의 결과는 " << big(c,d) << endl;
 }
